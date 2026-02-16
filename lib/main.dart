a318a6b2a6a66fea6335662e17e9bad4e9c9fd75
Azizur-K <c2080005@hallam.shu.ac.uk>
2026-02-16 13:34:07 +0000

    Added navbar & state management for home screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:elaros_mobile_app/app.dart';
+import 'package:provider/provider.dart';
+import 'package:elaros_mobile_app/ui/counter/view_models/counter_view_model.dart';
 
 void main() {
-  runApp(const App());
+  runApp(
+    ChangeNotifierProvider(
+      create: (_) => CounterViewModel(counter: 0),
+      child: const App(),
+    ),
+  );
 }
---
+++ lib/ui/common/widgets/bottom_navbar.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+class BottomNavBar extends StatelessWidget {
+  final int selectedIndex;
+  final Function(int) onTap;
+
+  const BottomNavBar({
+    super.key,
+    required this.selectedIndex,
+    required this.onTap,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return BottomNavigationBar(
+      items: const <BottomNavigationBarItem>[
+        BottomNavigationBarItem(
+          icon: Icon(Icons.home),
+          label: 'Home',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.health_and_safety),
+          label: 'Health Tips',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.favorite),
+          label: 'HR Zone',
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.person),
+          label: 'Profile',
+        ),
+      ],
+      currentIndex: selectedIndex,
+      onTap: onTap,
+    );
+  }
+}

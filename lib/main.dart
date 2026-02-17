b722188e476b5238c0b4f0ede51a95bb22c401ad
Azizur-K <c2080005@hallam.shu.ac.uk>
2026-02-17 11:15:18 +0000

    Added colour to NavBar for readability

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:elaros_mobile_app/app.dart';
 import 'package:provider/provider.dart';
 import 'package:elaros_mobile_app/ui/counter/view_models/counter_view_model.dart';
 
+
 void main() {
   runApp(
     ChangeNotifierProvider(

9f3e75ee04af99c3b6a6ea996af478cd6688411e
BillyBourbon <ballinson18@gmail.com>
2026-02-17 09:13:51 +0000

    extracted base graph creation

--- lib/ui/core/graphs/bar_chart.dart
+++ lib/ui/core/graphs/bar_chart.dart
@@ -1,48 +1,48 @@
-import 'package:cristalyse/cristalyse.dart';
 import 'package:flutter/material.dart';
+import 'package:cristalyse/cristalyse.dart';
+import 'package:elaros_mobile_app/ui/core/graphs/base_chart.dart';
 
-class BarChart extends StatelessWidget {
-  final List<Map<String, dynamic>> data;
-  final String mappingKeyX;
-  final String mappingKeyY;
-  final String mappingKeyColour;
+class BarChart extends BaseChart {
   final BarStyle barStyle;
-  final bool darkTheme;
-  final LegendPosition legendPosition;
+  final bool horizontalBars;
+  final BorderRadius borderRadius;
+  final double barWidth;
+  final double barAlpha;
+  final bool roundOutwardEdges;
+  final double borderWidth;
 
-  /// [data] is a list of maps, where each map represents a row in the table.
-  /// [mappingKeyX] is the key of the row that will be used as the x-axis.
-  /// [mappingKeyY] is the key of the row that will be used as the y-axis.
-  /// [mappingKeyColour] is the key of the row that will be used to assign the colour.
-  /// [barStyle] is the style of the bars.
-  /// [darkTheme] is a boolean that determines whether the chart will use the
-  /// dark theme or not.
-  /// [legendPosition] is the position of the legend.
   const BarChart({
     super.key,
-    required this.data,
-    required this.mappingKeyX,
-    required this.mappingKeyY,
-    required this.mappingKeyColour,
+    required super.data,
+    required super.mappingKeyX,
+    required super.mappingKeyY,
+    super.mappingKeyColour,
+    super.darkTheme,
+    super.legendPosition,
+    super.labelXMap,
+    super.labelYMap,
     this.barStyle = BarStyle.grouped,
-    this.darkTheme = true,
-    this.legendPosition = LegendPosition.bottom,
+    this.horizontalBars = false,
+    this.borderRadius = const BorderRadius.vertical(top: Radius.circular(8)),
+    this.barWidth = 0.4,
+    this.barAlpha = 0.7,
+    this.roundOutwardEdges = true,
+    this.borderWidth = 1.0,
   });
 
   @override
   Widget build(BuildContext context) {
-    return CristalyseChart()
-        .data(data)
-        .mapping(x: mappingKeyX, y: mappingKeyY, color: mappingKeyColour)
-        .geomBar(width: 0.4, alpha: 0.7, style: barStyle)
-        .scaleXOrdinal()
-        .scaleYContinuous(min: 0)
-        .theme(
-          darkTheme
-              ? ChartTheme.solarizedDarkTheme()
-              : ChartTheme.solarizedLightTheme(),
-        )
-        .legend(position: legendPosition)
-        .build();
+    CristalyseChart chart = buildChart().geomBar(
+      width: barWidth,
+      alpha: barAlpha,
+      style: barStyle,
+      borderRadius: borderRadius,
+      roundOutwardEdges: roundOutwardEdges,
+      borderWidth: borderWidth,
+    );
+
+    if (horizontalBars) chart.coordFlip();
+
+    return chart.build();
   }
 }

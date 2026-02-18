32674e8b617335e39f9e903a9c90f0d6b85040de
Stranger-786 <mysha06@hotmail.com>
2026-02-18 10:15:13 +0000

    Mysha-added-Profile-Page

---
+++ lib/utilities/category_card.dart
@@ -0,0 +1,42 @@
+/*
+import 'package:flutter/material.dart';
+
+class Category extends StatelessWidget {
+  final iconImagePath;
+  final String categoryName;
+
+  CategoryCard({
+    required this.icon,
+    required this.catergoryName,
+  })
+
+
+  const Category({Key? key}) : super(key: key);
+  @override
+  Widget build (BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(left:25.0),
+      child: Container (
+        padding: const EdgeInsets.all(20),
+        color: Colors.red[100],
+        decoration: BoxDecoration (
+          borderRadius: BorderRadius.circular(12),
+        color: Colors.white,
+      ),
+        child: Row(
+          children: [
+            Image.asset(
+              'lib/icons/yourgoalpp.png'
+              height: 30,
+            ),
+            SizedBox(
+              width:25,
+            ),
+            Text('yourgoals'),
+          ],
+          ),
+   ),
+    );
+  }
+}
+*/
